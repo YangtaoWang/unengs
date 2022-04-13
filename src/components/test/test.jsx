66a1b84fe6c664d825fdadd7e17c415d30e6851f
wangyangtao <wangyangtao@xdf.cn>
2022-04-13 18:43:47 +0800

    feat: 测试7

--- src/components/test/test.jsx
+++ src/components/test/test.jsx
@@ -2,7 +2,7 @@
  * @Author: wangyangtao@xdf.cn
  * @Date: 2022-04-13 16:56:52
  * @LastEditors: wangyangtao@xdf.cnn
- * @LastEditTime: 2022-04-13 17:05:21
+ * @LastEditTime: 2022-04-13 18:43:36
  * @Description: 描述信息
  */
 import React from 'react';
@@ -12,7 +12,7 @@ import PropTypes from 'prop-types';
 
 const TextView = ({ children }) => {
   return <div className='text-view'>
-    测试组件{children}
+    测试组件{children}测试组件
   </div>
 }
 
