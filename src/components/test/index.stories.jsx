75338623a48da216e00b206de7d30bb627aea07a
wangyangtao <wangyangtao@xdf.cn>
2022-04-13 20:03:23 +0800

    feat: 修改路径

--- src/components/test/index.stories.jsx
+++ src/components/test/index.stories.jsx
@@ -2,11 +2,11 @@
  * @Author: wangyangtao@xdf.cn
  * @Date: 2022-04-13 16:57:06
  * @LastEditors: wangyangtao@xdf.cnn
- * @LastEditTime: 2022-04-13 17:10:27
+ * @LastEditTime: 2022-04-13 20:03:01
  * @Description: 描述信息
  */
 import React from "react";
-import TextView from "./test.jsx";
+import TextView from "./index";
 
 export default {
   title: "Example/TextView",
