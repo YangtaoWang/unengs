f5ce1dd6f9e5b1e605ba3a54a37a8cfffb2661de
wangyangtao <wangyangtao@xdf.cn>
2022-04-19 11:07:57 +0800

    feat: 配置

--- postcss.config.js
+++ postcss.config.js
@@ -2,23 +2,13 @@
  * @Author: wangyangtao@xdf.cn
  * @Date: 2022-04-13 20:31:54
  * @LastEditors: wangyangtao@xdf.cnn
- * @LastEditTime: 2022-04-13 20:31:54
+ * @LastEditTime: 2022-04-18 15:42:12
  * @Description: 描述信息
  */
 module.exports = {
     plugins: [
       require("autoprefixer")({
-        browsers: [
-          "last 2 versions",
-          "Android >= 4.4",
-          "Firefox ESR",
-          "not ie < 9",
-          "ff >= 30",
-          "chrome >= 34",
-          "safari >= 6",
-          "opera >= 12.1",
-          "ios >= 6"
-        ]
+        overrideBrowserslist: ['> 0.15% in CN']
       })
     ]
 };
